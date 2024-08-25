da671fea9db7176914b91af77c2e7650f2116329
betty5634 <g820255@gmail.com>
2024-08-25 12:44:05 +0530

    cors

--- functions/app.js
+++ functions/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors");
 
 const app = express();
 const router = express.Router();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use(express.json());
 
